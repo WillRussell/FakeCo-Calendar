6a2d2428dc3d502942f29a2fc2bcd1604832abb8
Will <wrussell35@gmail.com>
2015-01-20 18:36:33 -0500

    navbar, clock added

--- app/app.js
+++ app/app.js
@@ -1,12 +1,33 @@
-'use strict';
+var app = angular.module('app', ['ui.bootstrap']);
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+app.controller('Ctrl2', ['$scope', function ($scope) {
+  $scope.format = 'h:mm a';
 }]);
+
+app.directive("myCurrentTime", function(dateFilter){
+    return function(scope, element, attrs){
+        var format;
+
+        scope.$watch(attrs.myCurrentTime, function(value) {
+            format = value;
+            updateTime();
+        });
+
+        function updateTime(){
+            var dt = dateFilter(new Date(), format);
+            element.text(dt);
+        }
+
+        function updateLater() {
+            setTimeout(function() {
+              updateTime(); // update DOM
+              updateLater(); // schedule another update
+            }, 1000);
+        }
+
+        updateLater();
+    }
+});
+
+}]);
+
