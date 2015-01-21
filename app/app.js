4c74339d8c9e0c5cb3ba4f4627d003f85d4d438d
Will <wrussell35@gmail.com>
2015-01-21 14:04:36 -0500

    functional ng-repeats

--- app/app.js
+++ app/app.js
@@ -1,17 +1,15 @@
 var app = angular.module('app', ['ui.bootstrap']);
 
-function DataCtrl($scope, $http) {
-
-  $http.jsonp("http://fake-co-calendar.herokuapp.com/api/v1/events?callback=JSON_CALLBACK").
-  success(function(data) {
-    $scope.data = data;
-    $scope.events = data.events;
-  }).
-  error(function (data) {
-    $scope.data = "Request failed";
+// hit remote server for meeting data
+function eventsCtrl($scope, $http) {
+var url = "http://fake-co-calendar.herokuapp.com/api/v1/events?callback=JSON_CALLBACK";
+$http.jsonp(url)
+  .success(function(data){
+      $scope.meetings = data.events.list
   });
-}
+};
 
+//navbar clock control
 app.controller('Ctrl2', ['$scope', function ($scope) {
   $scope.format = 'h:mm a';
 }]);
@@ -41,77 +39,5 @@ app.directive("myCurrentTime", function(dateFilter){
     }
 });
 
-app.controller('accordionCtrl', function ($scope) {
-  $scope.oneAtATime = true;
 
-  $scope.groups = [
-    {
-      title: 'Dynamic Group Header - 1',
-      content: 'Dynamic Group Body - 1'
-    },
-    {
-      title: 'Dynamic Group Header - 2',
-      content: 'Dynamic Group Body - 2'
-    }
-  ];
-
-  $scope.status = {
-    isFirstOpen: true,
-    isFirstDisabled: true
-  };
-});
-
-app.controller('meetingsCtrl', ['$scope', '$http', function($scope, $http){
-
-
-  // set collapse options for dynamic accordian
-  $scope.oneAtATime = true;
-  $scope.groups = [
-    {
-      title: 'Dynamic Group Header - 1',
-      content: 'Dynamic Group Body - 1'
-    },
-    {
-      title: 'Dynamic Group Header - 2',
-      content: 'Dynamic Group Body - 2'
-    }
-  ];
-  $scope.status = {
-    isFirstOpen: false,
-    isFirstDisabled: false
-  };
-
-
-
-
-
-  // setup client object to take data
-  // initializeCtrl();
-  // $scope.meeting = {
-  //   startTime: undefined,
-  //   endTime: undefined,
-  //   name: undefined,
-  //   location: undefined,
-  //   attendees: undefined,
-  //   toggle: function(){
-  //     $scope.client.showingDetails = !$scope.client.showingDetails;
-  //   }
-  // }
-  // function meetingsLoaded (data){
-  //   console.log(data);
-  //   $scope.client.name = data.node_label;
-  //   $scope.client.regions = data.children;
-  // };
-
-  // // hit facility tree api, grab data
-  // function initializeCtrl() {
-  //   $http({method: 'GET', url: 'http://fake-co-calendar.herokuapp.com/api/v1/events'})
-  //     .success(meetingsLoaded)
-
-  //     .error(function (data, status, headers, config) {
-  //       console.log('error!', status)
-  //       console.log(data)
-  //     });
-  //   };
-}]);
 
